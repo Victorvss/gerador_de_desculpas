bb39d74bfec29ef11626ab5736363713f1f58407
Victor <victorsants360@gmail.com>
2016-07-24 09:32:26 -0300

    Cadastro de Perfil

---
+++ app/controllers/admin/people_controller.rb
@@ -0,0 +1,52 @@
+class Admin::PeopleController < ApplicationController
+  before_action :set_person, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @people = Person.all
+  end
+
+  def show
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def edit
+  end
+
+  def create
+    @person = Person.new(person_params)
+
+
+      if @person.save
+        redirect_to [:admin,@person], notice: 'Perfil criado com sucesso.'
+      else
+        format.html { render :new }
+      end
+  end
+
+  def update
+
+      if @person.update(person_params)
+        redirect_to [:admin,@person], notice: 'Perfil atualizado com sucesso.'
+      else
+        format.html { render :edit }
+      end
+  end
+
+  def destroy
+    @person.destroy
+    redirect_to admin_people_url, notice: 'Perfil deletado com sucesso.'
+  end
+
+  private
+
+    def set_person
+      @person = Person.find(params[:id])
+    end
+
+    def person_params
+      params.require(:person).permit(:name)
+    end
+end
